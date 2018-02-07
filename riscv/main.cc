2a1766db98c1df40d7adf350ed18f0b802aff89a
Eli Boling <eli@dovermicrosystems.com>
2018-02-07 10:26:54 -0500

    Added exception wrappers to prevent C++ exceptions from taking down renode.

--- riscv/main.cc
+++ riscv/main.cc
@@ -4,18 +4,39 @@
 
 rv32_validator_t *validator;
 
+static bool DOA = false;
+
 extern "C" void e_v_set_callbacks(RegisterReader_t reg_reader, MemoryReader_t mem_reader) {
-  printf("setting callbacks\n");
-  validator = new rv32_validator_t(reg_reader, mem_reader);
+  try {
+    printf("setting callbacks\n");
+    validator = new rv32_validator_t(reg_reader, mem_reader);
+  } catch (...) {
+    printf("c++ exception while setting callbacks - policy code DOA\n");
+    DOA = true;
+  }
 }
 
 extern "C" uint32_t e_v_validate(uint32_t pc, uint32_t instr) {
 //  printf("validating 0x%x: 0x%x\n", pc, instr);
-  return validator->validate(pc, instr);
-//  return 1;
+  if (!DOA) {
+    try {
+      return validator->validate(pc, instr);
+    } catch (...) {
+      printf("c++ exception while validating - policy code DOA\n");
+      DOA = true;
+    }
+  }
+  return 0;
 }
 
 extern "C" void e_v_commit() {
 //  printf("committing\n");
-  validator->commit();
+  if (!DOA) {
+    try {
+      validator->commit();
+    } catch (...) {
+      printf("c++ exception while commiting - policy code DOA\n");
+      DOA = true;
+    }
+  }
 }
