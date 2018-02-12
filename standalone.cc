3b4c35a79a28507d16f44b5c2081242d65986413
Eli Boling <eli@dovermicrosystems.com>
2018-02-12 16:35:16 -0500

    Converted standalone app to a decoupled RV32 simulator.

--- standalone.cc
+++ standalone.cc
@@ -2,16 +2,52 @@
 
 #include "validator_exception.h"
 #include "soc_tag_configuration.h"
+#include "rv32_validator.h"
 
-int main() {
+meta_set_cache_t ms_cache;
+meta_set_factory_t *ms_factory;
+rv32_validator_t *rv_validator;
+
+static uint32_t regs[32];
+
+uint32_t reg_reader(uint32_t regno) { return regs[regno]; }
+
+void init() {
   try {
-  meta_set_cache_t mc;
-  meta_set_factory_t msf(&mc, "/home/eli/src/policy-engine/policy");
-  soc_tag_configuration_t soc(&msf, "soc_cfg.yml");
-  for (auto &e: soc) {
-    printf("start = 0x%x, end = 0x%x\n", e.start, e.end);
-  }
+    ms_factory = new meta_set_factory_t(&ms_cache, getenv("GENERATED_POLICY_DIR"));
+    soc_tag_configuration_t *soc_config =
+      new soc_tag_configuration_t(ms_factory,
+				  std::string(getenv("GENERATED_POLICY_DIR")) + "/../soc_cfg.yml");
+    rv_validator = new rv32_validator_t(&ms_cache, ms_factory, soc_config, reg_reader);
   } catch (validator::exception_t &e) {
     printf("exception: %s\n", e.what().c_str());
+  }
+}
+
+struct register_change_t {
+  int regno;
+  uint32_t new_value;
+  register_change_t(int regno, uint32_t new_value) : regno(regno), new_value(new_value) { }
+};
+
+struct op_t {
+  uint32_t pc;
+  uint32_t insn;
+  std::vector<register_change_t> changes;
+};
+
+#define RA(v) register_change_t(1, (v))
+
+std::vector<op_t> ops= {
+  { 0x80000200, 0x00000093, { RA(0) }}
+};
+
+int main() {
+  init();
+  for (auto &op: ops) {
+    rv_validator->validate(op.pc, op.insn);
+    for (auto &rc: op.changes) {
+      regs[rc.regno] = rc.new_value;
+    }
   }
 }
