c28ee4c46326b04902fcf2eaec326c696bf9a769
Eli Boling <eli@dovermicrosystems.com>
2018-03-26 08:22:33 -0400

    Added md_entity tool

---
+++ tagging_tools/md_entity.cc
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <gflags/gflags.h>
+
+//#include <experimental/filesystem>
+
+#include "basic_elf_io.h"
+#include "elf_loader.h"
+#include "symbol_table.h"
+#include "entity_binding.h"
+#include "validator_exception.h"
+#include "elf_utils.h"
+#include "metadata_tool.h"
+
+using namespace policy_engine;
+
+DEFINE_int32(base_address, 0x80000000, "Base address for memory map");
+DEFINE_bool(update, true, "update existing tag info file");
+
+static symbol_t *get_symbol(symbol_table_t const *symtab, reporter_t *err, std::string name, bool needs_size) {
+  symbol_t *sym = symtab->find_symbol(name);
+  if (sym) {
+    if (needs_size && sym->get_size() == 0) {
+      err->error("symbol %s has zero size.\n", name.c_str());
+      sym = nullptr;
+    }
+  } else {
+    err->error("symbol %s not found\n", name.c_str());
+  }
+  return sym;
+}
+
+int main(int argc, char **argv) {
+  stdio_reporter_t err;
+  gflags::SetUsageMessage("md_entity usage: md_entity <flags> policy_dir elf_file tag_info_file entity_map_yml\n");
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  const char *policy_dir = argv[1];
+  const char *elf_file_name = argv[2];
+  const char *tag_file_name = argv[3];
+  std::string entity_yml = std::string(policy_dir) + "/entities.yml";
+
+  try {
+    FILE *elf_in;
+
+    metadata_tool_t md_tool(policy_dir, FLAGS_base_address);
+    elf_in = fopen(elf_file_name, "rb");
+    FILE_reader_t reader(elf_in);
+    elf_image_t img(&reader, &err);
+    symbol_table_t symtab;
+    img.load();
+    populate_symbol_table(&symtab, &img);
+
+    if (FLAGS_update) {
+      if (!md_tool.load_tag_info(tag_file_name)) {
+	err.error("couldn't load tags from %s\n", tag_file_name);
+	return 2;
+      }
+    }
+
+#if 0
+    for (auto s: symtab.sorted_symbols()) {
+      printf("%s\n", s->get_name().c_str());
+    }
+    symbol_t *sym = symtab.find_symbol("ucHeap");
+    if (sym)
+      printf("found malloc at 0x%08x\n", sym->get_address());
+#endif
+    std::list<std::unique_ptr<entity_binding_t>> bindings;
+    load_entity_bindings(entity_yml.c_str(), bindings);
+    for (auto &e: bindings) {
+      entity_symbol_binding_t *sb = dynamic_cast<entity_symbol_binding_t *>(e.get());
+      if (sb != nullptr) {
+	symbol_t *sym = get_symbol(&symtab, &err, sb->elf_name, !sb->is_singularity);
+	if (sym) {
+	  // go ahead and mark it
+	  address_t end_addr;
+	  if (sb->is_singularity)
+	    end_addr = sym->get_address() + PLATFORM_WORD_SIZE;
+	  else
+	    end_addr = sym->get_address() + sym->get_size(); // TODO: align to platform word boundary?
+	  if (!md_tool.apply_tag(sym->get_address(), end_addr, sb->entity_name.c_str())) {
+	    err.warning("Unable to apply tag %s\n", sb->entity_name.c_str());
+	  }
+	}
+      } else {
+	entity_range_binding_t *rb = dynamic_cast<entity_range_binding_t *>(e.get());
+	symbol_t *sym = get_symbol(&symtab, &err, rb->elf_start_name, false);
+	symbol_t *end = get_symbol(&symtab, &err, rb->elf_end_name, false);
+	if (sym && end) {
+	  if (!md_tool.apply_tag(sym->get_address(), end->get_address(), rb->entity_name.c_str())) {
+	    err.warning("Unable to apply tag %s\n", rb->entity_name.c_str());
+	  }
+	}
+      }
+    }
+    if (err.errors == 0) {
+      if (!md_tool.save_tag_info(tag_file_name)) {
+	err.error("couldn't save tags to %s\n", tag_file_name);
+      }
+    }
+  } catch (std::exception &e) {
+    err.error("exception: %s\n", e.what());
+  }
+
+  return err.errors;
+}
