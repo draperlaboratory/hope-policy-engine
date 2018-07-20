ebb173b18c344eaa766a7325a27fd4fce7164323
Austin Harris <austinharris@utexas.edu>
2018-07-20 16:40:04 -0500

    Fix comparison for null pointers

--- validator/pipe/base_pipe.h
+++ validator/pipe/base_pipe.h
@@ -5,6 +5,14 @@
 
 struct compare_ops {
   bool operator()(const operands_t &a, const operands_t &b) const {
+
+    if (a.op1 && !b.op1) return false;
+    if (b.op1 && !a.op1) return false;
+    if (a.op2 && !b.op2) return false;
+    if (b.op2 && !a.op2) return false;
+    if (a.mem && !b.mem) return false;
+    if (b.mem && !a.mem) return false;
+
     if (a.op1 && b.op1) {
       if (a.op1->tags[0]!=b.op1->tags[0])
         return false;
