73990e0bb5c15d2bf4dc4572299e28c488344e5e
Scott Brookes <sbrookes@draper.com>
2019-04-30 05:24:17 -0400

    Specify loader for pyyaml in order to avoid warning

--- validator/riscv/rv32_validator.cc
+++ validator/riscv/rv32_validator.cc
@@ -341,10 +341,6 @@ void rv32_validator_t::prepare_eval(address_t pc, insn_bits_t insn) {
       if (flags & HAS_IMM)
         mem_addr += imm;
 
-      // metadata memory map can only handle aligned addrs, so treat this
-      //   like an aligned addr.
-      //      if ( mem_addr & (uintptr_t)0x3 )
-	//	printf("warning: misaligned addr 0x%x @ pc 0x%x\n", pc, mem_addr);
       mem_addr &= ~((uintptr_t)0x3);
     }
     ctx->bad_addr = mem_addr;
