5c03452beb09a4735e67c8e55a5fdeddefafa1e7
John Merrill <jmerrill@draper.com>
2019-09-05 12:38:46 -0400

    Add SOC ranges to range file

---
+++ tagging_tools/SOCTagger.py
@@ -0,0 +1,21 @@
+import yaml
+import TaggingUtils
+
+def generate_soc_ranges(soc_file, range_file, policy_inits):
+    soc_cfg = yaml.load(soc_file.read(), Loader=yaml.FullLoader)
+
+    soc_ranges = {}
+    for elem in soc_cfg['SOC']:
+        elem_fields = soc_cfg['SOC'][elem]
+        name = elem_fields['name']
+        start = elem_fields['start']
+        end = elem_fields['end']
+        soc_ranges[name] = (start, end)
+
+    # filter SOC range entries by those required by the policy init
+    policy_soc = policy_inits['Require']['SOC']
+    for device_type in policy_soc:
+        for elem in policy_soc[device_type]:
+            name = "SOC.{}.{}".format(device_type, elem)
+            if name in soc_ranges:
+                range_file.write_range(soc_ranges[name][0], soc_ranges[name][1], name)
