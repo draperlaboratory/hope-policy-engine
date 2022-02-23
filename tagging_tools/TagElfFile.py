064a631c32793e137199e580208e0ed3b83ae153
Alec Roelke <aroelke@draper.com>
2022-02-23 10:12:47 -0500

    Fix tag_array update to always output correct binary

--- tagging_tools/TagElfFile.py
+++ tagging_tools/TagElfFile.py
@@ -27,7 +27,7 @@ def generate_tag_array(elfname, range_file, policy_name, policy_meta_info, rv64)
     pout = subprocess.check_output([tool_prefix + 'objdump', '-h', elfname])
 
     if ".tag_array" in str(pout): # section exists, update the elf
-        base_command = tool_prefix + "objcopy --target=" + bfd_target + " --update-section .tag_array=" + tag_array_filename + " " + elfname + " " + elfname
+        base_command = tool_prefix + "objcopy --target=" + bfd_target + " --update-section .tag_array=" + tag_array_filename + " " + elfname + " " + elfname + "-" + policy_name
     else:
         base_command = tool_prefix + "objcopy --target=" + bfd_target + " --add-section .tag_array=" + tag_array_filename + " --set-section-flags .tag_array=readonly,data " + elfname + " " + elfname + "-" + policy_name
 
