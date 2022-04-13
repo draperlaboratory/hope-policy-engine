35fa763595631b3848e5fced4bb6f7520afb85cf
Alec Roelke <aroelke@draper.com>
2022-04-13 14:27:47 -0400

    Move tagging_utils class declarations to header

--- tagging_tools/tagging_utils.cc
+++ tagging_tools/tagging_utils.cc
@@ -9,58 +9,9 @@
 #include <unistd.h>
 #include <utility>
 #include <vector>
+#include "tagging_utils.h"
 
 namespace policy_engine {
-
-struct range_t {
-  uint64_t start;
-  uint64_t end;
-  std::vector<std::string>* tags;
-};
-
-class RangeFile {
-private:
-  int _fd;
-  std::string _name;
-public:
-  RangeFile() : _name("/tmp/range_XXXXXX") {
-    _fd = mkstemp((char*)_name.data());
-  }
-
-  size_t write_range(uint64_t start, uint64_t end, const std::string& tag);
-  void finish();
-  std::string name();
-  int fd();
-  void done();
-  void print();
-
-  ~RangeFile() {
-    if (_fd >= 0) {
-      done();
-      finish();
-    }
-  }
-};
-
-class RangeMap {
-private:
-  std::vector<range_t> range_map;
-public:
-  RangeMap() {}
-
-  bool contains(const range_t& key);
-  range_t& operator [](int i);
-  void sort();
-  void add_range(uint64_t start, uint64_t end, const std::string& tag);
-  void merge_ranges();
-  const std::vector<std::string>* get_tags(uint64_t addr);
-  std::vector<std::pair<uint64_t, uint64_t>>* get_ranges(const std::string& tag);
-
-  ~RangeMap() {
-    for (auto& range : range_map)
-      delete range.tags;
-  }
-};
 
 size_t RangeFile::write_range(uint64_t start, uint64_t end, const std::string& tag) {
   char buf[tag.length() + 48];
---
+++ tagging_tools/tagging_utils.h
@@ -0,0 +1,62 @@
+#ifndef __TAGGING_TOOLS_H__
+#define __TAGGING_TOOLS_H__
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace policy_engine {
+
+struct range_t {
+  uint64_t start;
+  uint64_t end;
+  std::vector<std::string>* tags;
+};
+
+class RangeFile {
+private:
+  int _fd;
+  std::string _name;
+public:
+  RangeFile() : _name("/tmp/range_XXXXXX") {
+    _fd = mkstemp((char*)_name.data());
+  }
+
+  size_t write_range(uint64_t start, uint64_t end, const std::string& tag);
+  void finish();
+  std::string name();
+  int fd();
+  void done();
+  void print();
+
+  ~RangeFile() {
+    if (_fd >= 0) {
+      done();
+      finish();
+    }
+  }
+};
+
+class RangeMap {
+private:
+  std::vector<range_t> range_map;
+public:
+  RangeMap() {}
+
+  bool contains(const range_t& key);
+  range_t& operator [](int i);
+  void sort();
+  void add_range(uint64_t start, uint64_t end, const std::string& tag);
+  void merge_ranges();
+  const std::vector<std::string>* get_tags(uint64_t addr);
+  std::vector<std::pair<uint64_t, uint64_t>>* get_ranges(const std::string& tag);
+
+  ~RangeMap() {
+    for (auto& range : range_map)
+      delete range.tags;
+  }
+};
+
+}
+
+#endif // __TAGGING_TOOLS_H__
