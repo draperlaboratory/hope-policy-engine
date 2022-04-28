f6abfa6526f7b281a534107381c41224ed33fe66
Alec Roelke <aroelke@draper.com>
2022-04-28 14:38:11 -0400

    Use range-based for loop

--- tagging_tools/elf_section_tagger.cc
+++ tagging_tools/elf_section_tagger.cc
@@ -12,22 +12,20 @@ static const std::string RWX_R = "elf.Section.SHF_ALLOC";
 static const std::string RWX_W = "elf.Section.SHF_WRITE";
 
 void generate_rwx_ranges(const elf_image_t& ef, RangeFile& range_file) {
-  for (int i = 0; i < ef.sections.size(); i++) {
-    uint64_t flags = ef.sections[i].flags;
-    uint64_t start = ef.sections[i].address;
-    uint64_t end = start + ef.sections[i].size;
+  for (const auto& section : ef.sections) {
+    uint64_t end = section.address + section.size;
     if (end % 4 != 0)
       end += 4 - (end % 4);
-    if (flags & SHF_EXECINSTR) {
-      range_file.write_range(start, end, RWX_X);
-      range_file.write_range(start, end, RWX_R);
-      std::printf("X %s: %#lx - %#lx\n", ef.sections[i].name.c_str(), start, end);
-    } else if (flags & SHF_WRITE) {
-      range_file.write_range(start, end, RWX_W);
-      std::printf("W %s: %#lx - %#lx\n", ef.sections[i].name.c_str(), start, end);
-    } else if (flags & SHF_ALLOC) {
-      range_file.write_range(start, end, RWX_R);
-      std::printf("R %s: %#lx - %#lx\n", ef.sections[i].name.c_str(), start, end);
+    if (section.flags & SHF_EXECINSTR) {
+      range_file.write_range(section.address, end, RWX_X);
+      range_file.write_range(section.address, end, RWX_R);
+      std::printf("X %s: %#lx - %#lx\n", section.name.c_str(), section.address, end);
+    } else if (section.flags & SHF_WRITE) {
+      range_file.write_range(section.address, end, RWX_W);
+      std::printf("W %s: %#lx - %#lx\n", section.name.c_str(), section.address, end);
+    } else if (section.flags & SHF_ALLOC) {
+      range_file.write_range(section.address, end, RWX_R);
+      std::printf("R %s: %#lx - %#lx\n", section.name.c_str(), section.address, end);
     }
   }
 }
