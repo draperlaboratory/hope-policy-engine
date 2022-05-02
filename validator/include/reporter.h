6e82c8bcc386138244fbc667af70ddf733df9fd7
Alec Roelke <aroelke@draper.com>
2022-05-02 11:41:32 -0400

    Simplify error reporter

--- validator/include/reporter.h
+++ validator/include/reporter.h
@@ -27,20 +27,41 @@
 #ifndef REPORTER_H
 #define REPORTER_H
 
+#include <cstdio>
+#include <string>
+
 namespace policy_engine {
 
 /**
    Generic reporter class, allowing error/informational messages to be channeled.
 */
 class reporter_t {
+private:
+  std::FILE* err;
+  std::FILE* warn;
+  std::FILE* out;
+
 public:
   int errors;
   int warnings;
 
-  reporter_t(int err_init=0, int warn_init=0) : errors(err_init), warnings(warn_init) {}
-  virtual void error(const char *fmt, ...) = 0;
-  virtual void warning(const char *fmt, ...) = 0;
-  virtual void info(const char *fmt, ...) = 0;
+  reporter_t(std::FILE* err=stderr, std::FILE* warn=stderr, std::FILE* out=stdout) : errors(0), warnings(0), err(err), warn(warn), out(out) {}
+
+  template<class... A>
+  void error(const char* fmt, const A&... args) {
+    std::fprintf(err, "error: ");
+    std::fprintf(err, fmt, args...);
+    errors++;
+  }
+
+  template<class... A>
+  void warning(const char* fmt, const A&... args) {
+    std::fprintf(warn, "warning: ");
+    std::fprintf(warn, fmt, args...);
+    warnings++;
+  }
+
+  template<class... A> void info(const char* fmt, const A&... args) { std::fprintf(out, fmt, args...); }
 };
 
 } // namespace policy_engine
