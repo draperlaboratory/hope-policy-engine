1fb9318c0b1e07e5868495f6940b04a6deac0e92
Alec Roelke <aroelke@draper.com>
2022-05-19 17:35:36 -0400

    md_code throws exception on failure instead of returning

--- tagging_tools/md_code_lib.cc
+++ tagging_tools/md_code_lib.cc
@@ -39,44 +39,33 @@
 
 namespace policy_engine {
 
-int md_code(const std::string& policy_dir, uint64_t code_address, const std::string& file_name, void* bytes, int n, reporter_t& err) {
-  try {
-    metadata_factory_t md_factory(policy_dir);
-    metadata_memory_map_t map;
-    if (!load_tags(map, file_name)) {
-      err.error("failed to read tags from %s\n", file_name);
-      return 1;
-    }
+void md_code(const std::string& policy_dir, uint64_t code_address, const std::string& file_name, void* bytes, int n, reporter_t& err) {
+  metadata_factory_t md_factory(policy_dir);
+  metadata_memory_map_t map;
+  if (!load_tags(map, file_name))
+    throw std::ios::failure("failure to read tags from " + file_name);
 
-    insn_bits_t* bits = reinterpret_cast<insn_bits_t*>(bytes);
-    for (int i = 0; i < n/sizeof(insn_bits_t); i++) {
-      decoded_instruction_t inst = decode(bits[i]);
-      if (!inst) {
-        err.warning("Failed to decode instruction 0x%08x at address %#x\n", bits[i], code_address);
-        code_address += 4;
-        continue;
-      }
-
-      std::shared_ptr<metadata_t> metadata = md_factory.lookup_group_metadata(inst.name, inst);
-
-      if (metadata == nullptr) {
-        err.warning("0x%016lx: 0x%08x  %s - no group found for instruction\n", code_address, bits[i], inst.name);
-      } else {
-        map.add_range(code_address, code_address + 4, metadata);
-      }
+  insn_bits_t* bits = reinterpret_cast<insn_bits_t*>(bytes);
+  for (int i = 0; i < n/sizeof(insn_bits_t); i++) {
+    decoded_instruction_t inst = decode(bits[i]);
+    if (!inst) {
+      err.warning("Failed to decode instruction 0x%08x at address %#x\n", bits[i], code_address);
       code_address += 4;
+      continue;
     }
 
-    if (!save_tags(map, file_name)) {
-      err.error("failed write of tag file\n");
-      return 1;
-    }
+    std::shared_ptr<metadata_t> metadata = md_factory.lookup_group_metadata(inst.name, inst);
 
-    return 0;
-  } catch (...) {
-    err.error("something awful happened\n");
-    return 1;
+    if (metadata == nullptr) {
+      err.warning("0x%016lx: 0x%08x  %s - no group found for instruction\n", code_address, bits[i], inst.name);
+    } else {
+      map.add_range(code_address, code_address + 4, metadata);
+    }
+    code_address += 4;
   }
+
+  if (!save_tags(map, file_name))
+    throw std::ios::failure("failed write of tag file");
 }
 
 } // namespace policy_engine
