f21555afca27dd9e42b58db14583b26e0570058b
Alec Roelke <aroelke@draper.com>
2022-06-10 10:21:53 -0400

    Restructure meta_cache_t code a bit

--- validator/riscv/meta_cache.h
+++ validator/riscv/meta_cache.h
@@ -27,52 +27,53 @@
 #ifndef META_CACHE_H
 #define META_CACHE_H
 
+#include <cstring>
 #include <memory>
-#include <string.h>
 #include <unordered_map>
-#include "policy_meta_set.h"
 #include "metadata.h"
+#include "policy_meta_set.h"
 
 namespace policy_engine {
 
 class meta_set_cache_t {
-  public:
+public:
   struct meta_set_hasher_t {
-    std::size_t operator()(const meta_set_t &k) const {
+    std::size_t operator ()(const meta_set_t& k) const {
       size_t hash = 0;
       for (int i = 0; i < META_SET_WORDS; i++)
-	hash += k.tags[i];
+	      hash += k.tags[i];
       return hash;
     }
   };
+
   struct meta_set_equal_t {
-    bool operator()(meta_set_t const &l, meta_set_t const &r) const {
+    bool operator ()(const meta_set_t& l, const meta_set_t& r) const {
       for (int i = 0; i < META_SET_WORDS; i++)
-	if (l.tags[i] != r.tags[i])
-	  return false;
+        if (l.tags[i] != r.tags[i])
+          return false;
       return true;
     }
   };
-  private:
-  std::unordered_map<meta_set_t, meta_set_t *, meta_set_hasher_t, meta_set_equal_t> map;
-  public:
-  meta_set_t const *canonize(meta_set_t const &ts) {
+
+private:
+  std::unordered_map<meta_set_t, std::unique_ptr<meta_set_t>, meta_set_hasher_t, meta_set_equal_t> map;
+
+public:
+  const meta_set_t* canonize(const meta_set_t& ts) {
     if (map.find(ts) == map.end()) {
-      meta_set_t *ms = new meta_set_t();
-      *ms = ts;
-      map[ts] = ms;
+      map[ts] = std::make_unique<meta_set_t>();
+      *map[ts] = ts;
     }
-    return map[ts];
+    return map[ts].get();
   }
-  meta_set_t const *canonize(metadata_t const *metadata) {
+
+  template<class MetaSetPtr>
+  const meta_set_t* canonize(MetaSetPtr metaset) {
     meta_set_t ms;
     memset(&ms, 0, sizeof(ms));
-    for (auto e: *metadata)
+    for (auto e: *metaset)
       ms_bit_add(&ms, e);
     return canonize(ms);
-  }
-  const meta_set_t* canonize(std::shared_ptr<const metadata_t> metadata) {
-    return canonize(metadata.get());
   }
 };
 
