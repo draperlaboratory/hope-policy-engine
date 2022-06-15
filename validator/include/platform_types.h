1cff17562bb5a99ebfa90cb0518b71d9c326357d
Alec Roelke <aroelke@draper.com>
2022-06-15 10:24:57 -0400

    "stdint.h" exists in C++, so no need for guard

--- validator/include/platform_types.h
+++ validator/include/platform_types.h
@@ -27,12 +27,8 @@
 #ifndef PLATFORM_TYPES_H
 #define PLATFORM_TYPES_H
 
-#ifdef __cplusplus
-  #include <cstdint>
-#else
-  #include "stdint.h"
-#endif
 #include <inttypes.h>
+#include <stdint.h>
 
 #define MIN_TAG_GRANULARITY 4
 
