6b6a5c9c33aad010b722275c2f81dedce821187c
Alec Roelke <aroelke@draper.com>
2022-06-15 09:56:34 -0400

    Reformat policy_eval.h

--- validator/include/policy-glue/policy_eval.h
+++ validator/include/policy-glue/policy_eval.h
@@ -36,76 +36,76 @@ extern "C" {
 namespace policy_engine {
 #endif
 
-#define POLICY_EXP_FAILURE 0
-#define POLICY_IMP_FAILURE -1
-#define POLICY_ERROR_FAILURE -2
-#define POLICY_SUCCESS 1
+enum policy_result_t {
+  POLICY_EXP_FAILURE = 0,
+  POLICY_IMP_FAILURE = -1,
+  POLICY_ERROR_FAILURE = -2,
+  POLICY_SUCCESS = 1
+};
+  
+/**
+ * Allocate and free memory for policy eval structures.
+ * Only need to do once, prepare eval should initialize each eval cycle
+ */
+void alloc_eval_params(context_t** ctx, operands_t** ops, results_t** res);
+void free_eval_params(context_t** ctx, operands_t** ops, results_t** res);
+
+/**
+ * Initialize context and set up operands before policy eval.
+ */
+void prepare_eval(context_t* ctx, operands_t* ops, results_t* res);
+
+/**
+ * Evaluate policy with context and operands, populate results.
+ *
+ * Returns status:
+ *    policyExpFailure = 0
+ *    policyImpFailure = -1
+ *    policySuccess = 1
+ */
+int eval_policy(context_t* ctx, operands_t* ops, results_t* res);
+
+/**
+ * Install rule with operands and results.
+ */
+void complete_eval(context_t* ctx, operands_t* ops, results_t* res);
+
+/**
+ * Helper Fn to optomize by returning a cannonical representation of
+ * a metadata set. Necessary for performance and to save memory.
+ */  
+meta_set_t* canonize(meta_set_t* ts);
 
   
-  /**
-   * Allocate and free memory for policy eval structures.
-   * Only need to do once, prepare eval should initialize each eval cycle
-   */
-void alloc_eval_params(context_t **ctx, operands_t **ops, results_t **res);
-void free_eval_params(context_t **ctx, operands_t **ops, results_t **res);
-
-  /**
-   * Initialize context and set up operands before policy eval.
-   */
-void prepare_eval(context_t *ctx, operands_t *ops, results_t *res);
-
-  /**
-   * Evaluate policy with context and operands, populate results.
-   *
-   * Returns status:
-   *    policyExpFailure = 0
-   *    policyImpFailure = -1
-   *    policySuccess = 1
-   */
-int eval_policy(context_t *ctx, operands_t *ops, results_t *res);
-
-  /**
-   * Install rule with operands and results.
-   */
-void complete_eval(context_t *ctx, operands_t *ops, results_t *res);
-
-  /**
-   * Helper Fn to optomize by returning a cannonical representation of
-   * a metadata set. Necessary for performance and to save memory.
-   */  
-meta_set_t *canonize(meta_set_t *ts);
-
+/**
+ * Print eval status
+ */
+void debug_msg(context_t* ctx, const char* msg);
   
-  /**
-   * Print eval status
-   */
-void debug_msg(context_t *ctx, const char *msg);
+/**
+ * Print eval status
+ */
+void debug_status(context_t* ctx, int status);
   
-  /**
-   * Print eval status
-   */
-void debug_status(context_t *ctx, int status);
+/**
+ * Print operands
+ */
+void debug_operands(context_t* ctx, operands_t* ops);
+
+/**
+ * Print results
+ */
+void debug_results(context_t* ctx, results_t* res);
+
+/**
+ * Call this if there is a rule violation
+ */
+void handle_violation(context_t* ctx, operands_t* ops, results_t* out);
   
-  /**
-   * Print operands
-   */
-void debug_operands(context_t *ctx, operands_t *ops);
-
-
-  /**
-   * Print results
-   */
-void debug_results(context_t *ctx, results_t *res);
-
-  /**
-   * Call this if there is a rule violation
-   */
-void handle_violation(context_t *ctx, operands_t *ops, results_t *out);
-  
-  /**
-   * Call this if there is a fatal error in the eval code
-   */
-void handle_panic(const char *msg);
+/**
+ * Call this if there is a fatal error in the eval code
+ */
+void handle_panic(const char* msg);
   
 #ifdef __cplusplus
 } // namespace policy_engine
@@ -113,4 +113,3 @@ void handle_panic(const char *msg);
 #endif
 
 #endif // POLICY_EVAL_H
-
